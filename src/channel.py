7ae3a8b9f0bd535c51f3bdb15c99d3031d0ca3e3
К <hitandrun444@yandex.ru>
2024-01-28 19:51:06 +0300

    add file

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,21 @@
 
+import json
+
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.__channel_id = channel_id
+        self.channel = self.get_channel_info(channel_id)
+        self.title = self.channel['snippet']["title"]
+        self.description = self.channel['snippet']["description"]
+        self.url = f"https://www.youtube.com/channel/{self.__channel_id}"
+        self.subscribers = self.channel["statistics"]["subscriberCount"]
+        self.video_count = self.channel["statistics"]["videoCount"]
+        self.view_count = self.channel["statistics"]["viewCount"]
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
