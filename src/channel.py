f3bac3a00296312b7c5890e32fae1f7f026cc5ae
К <hitandrun444@yandex.ru>
2024-01-30 16:46:28 +0300

    add file

--- src/channel.py
+++ src/channel.py
@@ -1,9 +1,12 @@
-
 import json
+import os
+from distutils.command.build import build
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
@@ -19,3 +22,26 @@ class Channel:
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        """Класс-метод возвращающий объект для работы с YouTube API."""
+        return cls.youtube
+
+    def to_json(self, file_name):
+        """Метод сохраняющий в файл значения атрибутов экземпляра Channel."""
+        channel_info = {
+            "channel_id": self.__channel_id,
+            "title": self.title,
+            "description": self.description,
+            "url": self.url,
+            "subscribers": self.subscribers,
+            "videoCount": self.video_count,
+            "viewCount": self.view_count,
+        }
+
+        json_data = json.dumps(channel_info, indent=2, ensure_ascii=False)
+
+        with open(file_name, "w", encoding="UTF-8") as outfile:
+            outfile.write(json_data)
+
